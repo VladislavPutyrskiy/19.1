0f1961c92d0d6e0c026a4bad4b2ca7e5246257f9
Vladislav Putyrskiy <V.V.Putyrskiy@gmail.com>
2022-08-03 00:31:55 +0300

    Добавьте файлы проекта.

---
+++ 19.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+int main()
+{   
+    int count = 0, aimcount = 0;
+    const std::string looked_for = "If";
+    std::ifstream fin;
+    fin.open("if.txt");
+    std::string a[300];
+    if (fin.is_open())
+    {
+        while (!fin.eof())
+        {
+            fin >> a[count];
+            count++;
+         }
+        for (int i = 0; i <=count; i++)
+        {
+            if (a[i] == looked_for)
+            {
+                aimcount++;
+            }
+        }
+        fin.close();
+    }
+    else
+    {
+        std::cout << "some problem";
+    }
+    std::cout << aimcount;
+}
